7371b920de5b796f594b0c90c18ae7b72d5fe862
Kwangil <kylekwp22@gmail.com>
2018-06-21 22:35:28 -0500

    implemented chart

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const express = require('express');
-
+const path = require('path');
+const Chart = require('chart.js');
 const app = express();
 
-app.get('/', function(req,res){
-	res.send('hello');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','html');
+
+app.get('/test', function(req,res){
+	res.send(`{
+	"plots": [{
+			"x": 1,
+			"y": 2
+		},
+		{
+			"x": 3,
+			"y": 4
+		},
+		{
+			"x": 5,
+			"y": 6
+		}
+	]
+}`);
 });
+
+app.get('/', function(req,res){
+	res.render('index',{});
+});
+
+
 
 app.listen(3000,function(){
 	console.log('server started on port 3000')
